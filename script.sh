b85fa36667c6be9eacf5020ebd05f1b048471f88
yaizafontanet <57803576+yaizafontanet@users.noreply.github.com>
2022-02-16 15:00:01 +0100

    repte--01

--- script.sh
+++ script.sh
@@ -4,10 +4,7 @@ sudo cd /var/www/html
 echo "MCLOUD" > hw.html                                                                                
 sudo wget https://es.wordpress.org/wordpress-latest-es_ES.tar.gz                                                 
 sudo tar -xzvf wordpress-latest-es_ES.tar.gz                                                                    
-sudo cp -r wordpress/* /var/www/html/
-sudo cd /var/www/html/wordpress/
-sudo cp wp-config-sample.php wp-config.php
-sudo cd                                                                  
+sudo cp -r wordpress/* /var/www/html/                                                               
 sudo rm -rf wordpress                                                                                   
 sudo rm -rf wordpress-latest-es_ES.tar.gz                                                                      
 sudo chmod -R 755 wp-content                                                                            
