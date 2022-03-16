a7b9053d2c10b2766f1b0af4ed11809d9130938d
yaizafontanet <57803576+yaizafontanet@users.noreply.github.com>
2022-03-16 17:28:37 +0100

    r1

--- script.sh
+++ script.sh
@@ -6,59 +6,117 @@ db_username="yaiza"
 db_user_password="yaiza12345"
 db_name="mcloud"
 db_RDS=${aws_db_instance.DataBase.endpoint}
+                                                                                          
+sudo yum install httpd php php-mysql -y -q                                                              
+sudo cd /var/www/html                                                                                   
+echo "MCLOUD" > hw.html                                                                                
+sudo wget https://es.wordpress.org/wordpress-latest-es_ES.tar.gz                                                 
+sudo tar -xzvf wordpress-latest-es_ES.tar.gz                                                                    
+sudo cp -r wordpress/* /var/www/html/                                                               
+sudo rm -rf wordpress                                                                                   
+sudo rm -rf wordpress-latest-es_ES.tar.gz                                                                      
+sudo chmod -R 755 wp-content                                                                            
+sudo chown -R apache:apache wp-content                                                                  
+sudo wget https://s3.amazonaws.com/bucketforwordpresslab-donotdelete/htaccess.txt                       
+sudo mv htaccess.txt .htaccess                                                                          
+sudo systemctl start httpd                                                                              
+systemctl enable httpd
+touch /var/www/html/wp-config.php
+cat > /var/www/html/wp-config.php << EOF
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
 
-# install LAMP Server
-apt update  -y
-apt upgrade -y
-apt update  -y
-apt upgrade -y
-#install apache server
-apt install -y apache2
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', '$db_name' );
 
-apt install -y php
-apt install -y php php-{pear,cgi,common,curl,mbstring,gd,mysqlnd,bcmath,json,xml,intl,zip,imap,imagick}
+/** Database username */
+define( 'DB_USER', '$db_username' );
+
+/** Database password */
+define( 'DB_PASSWORD', '$db_user_password' );
+
+/** Database hostname */
+define( 'DB_HOST', '$db_RDS' );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* Add any custom values between this line and the "stop editing" line. */
 
 
 
-#and download mysql package to yum  and install mysql client from yum
-apt install -y mysql-client-core-8.0
+/* That's all, stop editing! Happy publishing. */
 
-# starting apache  and register them to startup
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
 
-systemctl enable --now  apache2
-
-
-# Change OWNER and permission of directory /var/www
-usermod -a -G www-data ubuntu
-chown -R ubuntu:www-data /var/www
-find /var/www -type d -exec chmod 2775 {} \;
-find /var/www -type f -exec chmod 0664 {} \;
-
-# Download wordpress package and extract
-wget https://wordpress.org/latest.tar.gz
-tar -xzf latest.tar.gz
-cp -r wordpress/* /var/www/html/
-# Create wordpress configuration file and update database value
-cd /var/www/html
-cp wp-config-sample.php wp-config.php
-sed -i "s/database_name_here/$db_name/g" wp-config.php
-sed -i "s/username_here/$db_username/g" wp-config.php
-sed -i "s/password_here/$db_user_password/g" wp-config.php
-sed -i "s/localhost/$db_RDS/g" wp-config.php
-cat <<EOF >>/var/www/html/wp-config.php
-define( 'FS_METHOD', 'direct' );
-define('WP_MEMORY_LIMIT', '128M');
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
 EOF
-
-# Change permission of /var/www/html/
-chown -R ubuntu:www-data /var/www/html
-chmod -R 774 /var/www/html
-rm /var/www/html/index.html
-#  enable .htaccess files in Apache config using sed command
-sed -i '/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride all/' /etc/apache2/apache2.conf
-a2enmod rewrite
-
-# restart apache
-
-systemctl restart apache2
-echo WordPress Installed
